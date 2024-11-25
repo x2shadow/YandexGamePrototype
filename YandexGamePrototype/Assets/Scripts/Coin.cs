3d468c91597585986e8fdf0d7c6967fb337440a4
X2SHADOW <hahandrew88@gmail.com>
2024-11-25 22:22:23 +0700

    Added Coin and Coin Manager

---
+++ YandexGamePrototype/Assets/Scripts/Coin.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    [SerializeField] float rotationSpeed = 80f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, rotationSpeed * Time.deltaTime, 0);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        FindObjectOfType<CoinManager>().AddOne();
+        Destroy(gameObject);
+    }
+}
