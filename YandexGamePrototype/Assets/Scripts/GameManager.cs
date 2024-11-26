b2c6dda1115922e4312dff92001cddfed231a52b
X2SHADOW <hahandrew88@gmail.com>
2024-11-26 16:04:07 +0700

    Added Level 2 and Level name UI

--- YandexGamePrototype/Assets/Scripts/GameManager.cs
+++ YandexGamePrototype/Assets/Scripts/GameManager.cs
@@ -1,10 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField] GameObject startMenu;
+    [SerializeField] TextMeshProUGUI levelText;
+
+    void Start()
+    {
+        levelText.text = SceneManager.GetActiveScene().name;
+    }
 
     public void Play()
     {
