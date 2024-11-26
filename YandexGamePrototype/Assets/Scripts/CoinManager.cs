54ed7998702d763d13506dda67a40a07e6bf0584
X2SHADOW <hahandrew88@gmail.com>
2024-11-26 11:10:43 +0700

    Added UI and Gates
    - Canvas(Coints)
    - Canvas(StartMenu)

--- YandexGamePrototype/Assets/Scripts/CoinManager.cs
+++ YandexGamePrototype/Assets/Scripts/CoinManager.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class CoinManager : MonoBehaviour
 {
     [SerializeField] int numberOfCoinsInLevel;
+    [SerializeField] TextMeshProUGUI coinNumberText;
     
     public void AddOne()
     {
         numberOfCoinsInLevel++;
+        coinNumberText.text = numberOfCoinsInLevel.ToString();
     }
 }
